1f31c0bac1c905e1d117ebe6adfd49352c6dbded
VinceIsWorking <93628200+VinceIsWorking@users.noreply.github.com>
2026-03-11 16:14:13 +1100

    Fix URL formatting in main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const portfolioContent = {
 🐞 “Ah, bugs,”
 🥲 he said, wiping his eyes.
 🧙‍♂️ “A magic beyond all we do here!”
-👨‍💻 he who is wholeheartedly developing https://www.aspectu4d.com/ using UE5/C++ for 4 years
+👨‍💻 he who is wholeheartedly developing www.aspectu4d.com using UE5/C++ for 4 years
 </p>
       <div class="card-grid">
         <div class="pixel-card"><strong>Base</strong> Melbourne, Australia</div>
